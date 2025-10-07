4ae3c5c4112770d5fdf021043eae564869fde775
Stephen_A <ankustephen99@gmail.com>
2025-10-07 14:34:35 -0400

    printed date/time with timezone

--- src/main/java/com/pluralsight/FormatDates.java
+++ src/main/java/com/pluralsight/FormatDates.java
@@ -7,11 +7,16 @@ import java.time.format.DateTimeFormatter;
 public class FormatDates {
     public static void main(String[] args) {
         LocalDateTime today = LocalDateTime.now();
-//        DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+        ZonedDateTime todayZoned = ZonedDateTime.now();
+        DateTimeFormatter myFormatObj0 = DateTimeFormatter.ofPattern("HH:mma z");
+        DateTimeFormatter myFormatObj1 = DateTimeFormatter.ofPattern("dd-MMM-yyyy");
         System.out.println(today.getMonthValue() + "/" + today.getDayOfMonth() + "/" + today.getYear());
         System.out.println(today.getYear() + "-" + today.getMonthValue() + "-" + today.getDayOfMonth());
         System.out.println(today.getMonth() + " " + today.getDayOfMonth() + ", " + today.getYear());
-        System.out.println(today.getDayOfWeek() + ", " + today.getMonth() + " " + today.getDayOfMonth() + ", " + today.getYear() + "  " );
+        System.out.println(today.getDayOfWeek() + ", " + today.getMonth() + " " + today.getDayOfMonth() + ", " + today.getYear() + "  " + todayZoned.format(myFormatObj0) );
+
+        System.out.println("CHALLENGE");
+        System.out.println(todayZoned.format(myFormatObj0) + " on " + today.format(myFormatObj1));
 
 
 //        System.out.println("");
