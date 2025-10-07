7bcaf70363e4c4050ed51f52a7e5e6a020c2276d
Stephen_A <ankustephen99@gmail.com>
2025-10-07 14:43:28 -0400

    Added date/time in GMT timezone

--- src/main/java/com/pluralsight/FormatDates.java
+++ src/main/java/com/pluralsight/FormatDates.java
@@ -1,6 +1,7 @@
 package com.pluralsight;
 
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 
@@ -8,12 +9,13 @@ public class FormatDates {
     public static void main(String[] args) {
         LocalDateTime today = LocalDateTime.now();
         ZonedDateTime todayZoned = ZonedDateTime.now();
+        ZonedDateTime todayGMT = ZonedDateTime.now(ZoneId.of("GMT"));
         DateTimeFormatter myFormatObj0 = DateTimeFormatter.ofPattern("HH:mma z");
         DateTimeFormatter myFormatObj1 = DateTimeFormatter.ofPattern("dd-MMM-yyyy");
         System.out.println(today.getMonthValue() + "/" + today.getDayOfMonth() + "/" + today.getYear());
         System.out.println(today.getYear() + "-" + today.getMonthValue() + "-" + today.getDayOfMonth());
         System.out.println(today.getMonth() + " " + today.getDayOfMonth() + ", " + today.getYear());
-        System.out.println(today.getDayOfWeek() + ", " + today.getMonth() + " " + today.getDayOfMonth() + ", " + today.getYear() + "  " + todayZoned.format(myFormatObj0) );
+        System.out.println(today.getDayOfWeek() + ", " + today.getMonth() + " " + today.getDayOfMonth() + ", " + today.getYear() + "  " + todayGMT.format(myFormatObj0));
 
         System.out.println("CHALLENGE");
         System.out.println(todayZoned.format(myFormatObj0) + " on " + today.format(myFormatObj1));
