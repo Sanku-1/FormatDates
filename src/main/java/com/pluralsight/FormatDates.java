d9f52311515723eb10d78d5b8f3a8d2967b93302
Stephen_A <ankustephen99@gmail.com>
2025-10-07 14:01:30 -0400

    printed day in MM/dd/YYYY format

--- src/main/java/com/pluralsight/FormatDates.java
+++ src/main/java/com/pluralsight/FormatDates.java
@@ -1,7 +1,17 @@
 package com.pluralsight;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class FormatDates {
     public static void main(String[] args) {
+        LocalDateTime today = LocalDateTime.now();
+//        DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+        System.out.println(today.getMonthValue() + "/" + today.getDayOfMonth() + "/" + today.getYear());
+//        System.out.println("");
+//        System.out.println("");
+//        System.out.println("");
+
 
     }
 }
